ef95170b20dea2de32829bc871f91316308d122e
asd <asd@192.168.120.65>
2020-11-18 17:45:58 +0900

    #04 패키지 명 변경 - hw

--- petpal/src/main/java/com/project/petpal/community/knowhow/controller/knowHowController.java
+++ petpal/src/main/java/com/project/petpal/community/knowhow/controller/knowHowController.java
@@ -1,4 +1,4 @@
-package com.project.petpal.knowhow.controller;
+package com.project.petpal.community.knowhow.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
