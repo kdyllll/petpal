7c1a241ff725a27b4acb0600ca3785b4418f9f6d
daye109 <daye109@nate.com>
2020-11-24 20:16:27 +0900

    admin 상품등록 작성중 -다예

--- petpal/src/main/java/com/project/petpal/admin/controller/AdminProductController.java
+++ petpal/src/main/java/com/project/petpal/admin/controller/AdminProductController.java
@@ -1,5 +1,11 @@
 package com.project.petpal.admin.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -18,30 +24,50 @@ public class AdminProductController {
 			@RequestParam(value="pdtPictures", required=false) MultipartFile[] upFile,
 			@RequestParam(value="fName", required = false) MultipartFile[] fName,
 			 HttpSession session) {
-		//productImg파일들
-		for(int i=0; i<upFile.length; i++) {
-			System.out.println(upFile[i].getOriginalFilename());
-		}	
-		//상품설명파일
-		for(int i=0; i<fName.length; i++) {
-			System.out.println(fName[i].getOriginalFilename());
+		//product 설명파일
+		String path = session.getServletContext().getRealPath("/resources/upload/product/detail");
+		File dir = new File(path);
+		if(!dir.exists()) dir.mkdirs();
+		
+		for(MultipartFile mf : fName) {
+			if (!mf.isEmpty()) {
+				// 파일명 생성
+				String originalName = mf.getOriginalFilename();
+				String ext = originalName.substring(originalName.lastIndexOf(".") + 1);
+
+				// 리네임 규칙
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+				int rndValue = (int) (Math.random() * 10000);
+				String reName = sdf.format(System.currentTimeMillis()) + "_" + rndValue + "." + ext;
+				try {
+					mf.transferTo(new File(path + "/" + reName));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				Product p = Product.builder().fileName(reName).build();
+				System.out.println("파일이름:"+p.getFileName());
+			}
 		}
 		
 		//사이즈,색구분
+		List<Stock> stockList = new ArrayList();
 		String colors[] = stock.getColor().split(",");
 		if(!stock.getProductSize().equals("") && !stock.getColor().equals("")) {
 			String sizes[] = stock.getProductSize().split(",");
 			for(String s : sizes) {
 				for(String c : colors) {			
-					System.out.println(s + ":" +c);
+					Stock st = Stock.builder().productSize(s).color(c).build();
+					stockList.add(st);
 				}	
 			}
 		} else if(stock.getProductSize().equals("") && !stock.getColor().equals("")) {
 			for(String c : colors) {			
-				System.out.println(c);
+				Stock st = Stock.builder().productSize(null).color(c).build();
+				stockList.add(st);
 			}	
 		} else {
-			System.out.println("색,사이즈 없음");
+			Stock st = Stock.builder().productSize(null).color(null).build();
+			stockList.add(st);
 		}
 		
 
